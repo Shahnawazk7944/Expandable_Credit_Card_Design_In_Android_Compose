b003c0879d224bf50b34bacd88c7bac4d44dd2ff
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2023-12-18 03:10:16 +0530

    Expandable credit design final commit

--- app/src/main/java/com/example/expendablecarddesign/ExpandableCreditCard.kt
+++ app/src/main/java/com/example/expendablecarddesign/ExpandableCreditCard.kt
@@ -1,26 +1,25 @@
 package com.example.expendablecarddesign
 
-import androidx.compose.animation.ContentTransform
+import android.media.Image
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.outlined.ArrowForward
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -35,33 +34,31 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.example.expendablecarddesign.ui.theme.CardBackgroundColor
+import com.example.expendablecarddesign.ui.theme.CardPayNowBackgroundColor
 import com.example.expendablecarddesign.ui.theme.PrimaryWhiteColor
 import com.example.expendablecarddesign.ui.theme.SecondaryDarkColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CreditCard() {
+fun CreditCard(cardType:Int ,cardNumber: String, name: String, validityMonth: String, validityYear: String, cvv: String) {
     var cardState by remember {
         mutableStateOf(false)
     }
+    val rotateState by animateFloatAsState(targetValue = if (cardState) 180f else 0f)
 
     Card(modifier = Modifier
-        .height(Dp(500f))
         .fillMaxWidth()
         .animateContentSize(
             animationSpec = tween(
-                delayMillis = 300, easing = LinearOutSlowInEasing
+                durationMillis = 300, easing = LinearOutSlowInEasing
             )
-
         ), colors = CardDefaults.cardColors(
         containerColor = CardBackgroundColor,
     ), shape = ShapeDefaults.Medium, onClick = {
@@ -70,37 +67,131 @@ fun CreditCard() {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(12.dp)
+                .padding(start = 20.dp, top = 20.dp, bottom = 20.dp),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Column(modifier = Modifier.padding(bottom = 10.dp)) {
-                Text(
-                    text = "Card Number",
-                    color = SecondaryDarkColor,
-                    style = MaterialTheme.typography.titleMedium
-                )
-                Row(
-                    modifier = Modifier.padding(top = 5.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceEvenly
+            Row(
+                verticalAlignment = Alignment.Top,
+            ) {
+                Column(
+                    modifier = Modifier.weight(5f)
                 ) {
                     Text(
-                        modifier = Modifier.weight(5f),
-                        text = "1234 1234 1234 1234",
-                        color = PrimaryWhiteColor,
-                        style = MaterialTheme.typography.titleLarge
+                        text = "Card Number",
+                        textAlign = TextAlign.Center,
+                        color = SecondaryDarkColor,
+                        style = MaterialTheme.typography.titleSmall
                     )
-                    IconButton(
-                        onClick = { cardState = !cardState }, modifier = Modifier.alpha(5f).weight(1f),
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowDropDown,
-                            contentDescription = "drop down Icon"
-                        )
-                    }
+                    Text(
+                        text = cardNumber,
+                        color = PrimaryWhiteColor,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+
+                }
+
+                Image(
+                    painter = painterResource(id = cardType),
+                    contentDescription = "Image",
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(50.dp)
+                        .weight(1f)
+
+                )
+                IconButton(
+                    onClick = { cardState = !cardState },
+                    modifier = Modifier
+                        .alpha(5f)
+                        .rotate(rotateState),
+                ) {
+
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "dropDownIcon"
+                    )
                 }
 
 
             }
+            if (cardState) {
+                Column(
+                    modifier = Modifier.padding(top = 20.dp),
+                ) {
+                    Text(
+                        text = "Cardholder Name",
+                        textAlign = TextAlign.Center,
+                        color = SecondaryDarkColor,
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                    Text(
+                        text = name,
+                        color = PrimaryWhiteColor,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+
+                }
+                Row(
+                    modifier = Modifier.padding(top = 20.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(modifier = Modifier.weight(2f)) {
+                        Text(
+
+                            text = "Valid thru",
+                            textAlign = TextAlign.Center,
+                            color = SecondaryDarkColor,
+                            style = MaterialTheme.typography.titleSmall
+                        )
+                        Text(
+
+                            text = "$validityMonth / $validityYear",
+                            color = PrimaryWhiteColor,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+
+                    }
+                    Column(modifier = Modifier.weight(2f)) {
+                        Text(
+                            text = "CVV",
+                            textAlign = TextAlign.Center,
+                            color = SecondaryDarkColor,
+                            style = MaterialTheme.typography.titleSmall
+                        )
+                        Text(
+                            text = cvv,
+                            color = PrimaryWhiteColor,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+
+                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.pay),
+                        contentDescription = "Image",
+                        modifier = Modifier
+                            .height(60.dp)
+                            .width(70.dp)
+                            .weight(1f)
+
+                    )
+                    Icon(modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 10.dp),
+                        imageVector = Icons.Filled.ArrowForward,
+                        contentDescription = "dropDownIcon",
+                        tint = CardPayNowBackgroundColor
+                    )
+                }
+            }
+
 
         }
 
@@ -112,5 +203,7 @@ fun CreditCard() {
 @Composable
 @Preview(showBackground = true)
 fun CreditCartPreview() {
-    CreditCard()
+    CreditCard(
+        cardNumber = "8169 2121 4100 5622" , name = "Shahnawaz Khan",
+        validityMonth = "11",validityYear = "2027", cvv = "548", cardType = R.drawable.mastercard)
 }
--- app/src/main/java/com/example/expendablecarddesign/MainActivity.kt
+++ app/src/main/java/com/example/expendablecarddesign/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.expendablecarddesign
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.expendablecarddesign.ui.theme.ExpendableCardDesignTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +22,40 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExpendableCardDesignTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(24.dp),
+                    verticalArrangement = Arrangement.SpaceAround
                 ) {
-                    CreditCard()
+                    CreditCard(
+                        cardNumber = "8169 2121 4100 5622",
+                        name = "Shahnawaz Khan",
+                        validityMonth = "11",
+                        validityYear = "2027",
+                        cvv = "548",
+                        cardType = R.drawable.mastercard
+                    )
+                    CreditCard(
+                        cardNumber = "9586 4621 5725 4476",
+                        name = "Jackson Sen",
+                        validityMonth = "04",
+                        validityYear = "2025",
+                        cvv = "795",
+                        cardType = R.drawable.visacard
+                    )
+                    CreditCard(
+                        cardNumber = "4495 2876 3157 9523",
+                        name = "Yamez Shizu",
+                        validityMonth = "09",
+                        validityYear = "2030",
+                        cvv = "201",
+                        cardType = R.drawable.rupaycard
+                    )
                 }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExpendableCardDesignTheme {
-        Greeting("Android")
     }
 }
